b7eca4a3bd419f5e3fa49f3dc74e43d56b9fdae0
Keiravi123 <vilakeira@gmail.com>
2025-01-15 16:26:35 -0500

    fix

--- react_1/Card.js
+++ react_1/Card.js
@@ -6,7 +6,7 @@ function Card(props) {
     <div classname="imagemy-style">
        {}
        <h2>{props.name}</h2>
-       <img src={props.img} alt="avatar_img"  />
+       <img src="tommy.png" alt="avatar_img"  />
       <p>{props.tel}</p>
       <p>{props.email}</p>
     </div>
